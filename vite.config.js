b4d545e61ef71adf775b6788d84aadcdc8b2b45f
elias roy <provinciaelias@gmail.com>
2025-09-15 21:24:51 -0500

    fix: url front sin https

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     allowedHosts: [
-      'https://medilink-frontend-latest.onrender.com/'
+      'medilink-frontend-latest.onrender.com' 
     ],
     proxy: {
       '/api': {
